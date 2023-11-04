6bb65a8fdbcf9564324387c6b313332d10d94201
freeluckperson <gestionecesaria@gmail.com>
2023-11-04 13:10:13 -0400

    primer commit

---
+++ pruebas/palindromo.js
@@ -0,0 +1,15 @@
+/*Dado un string comprobar si es PALINDROMO. Palindromo: son palabras que se leen de izquierda a derecha y viceversa ejemplo => ana */
+
+function espalindromo(palabras) {
+  let palabraInvertida = "";
+  for (let letter of palabras) {
+    palabraInvertida = letter + palabraInvertida;
+  }
+  console.log(palabraInvertida);
+  if (palabraInvertida === palabras) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = espalindromo;
