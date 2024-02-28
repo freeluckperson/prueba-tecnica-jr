9c43317be9489c13095f96c7bd8bdd2f25d59e57
erickubuntu <gestionecesaria@gmail.com>
2024-02-28 12:24:48 -0400

    28 feb 2024

--- pruebas/index.js
+++ pruebas/index.js
@@ -17,12 +17,30 @@
 // console.log(products);
 
 // Formatear fechas en javascript
-const format = (date, locale, options) =>
-  new Intl.DateTimeFormat(locale, options).format(date);
+// const format = (date, locale, options) =>
+//   new Intl.DateTimeFormat(locale, options).format(date);
 
-const now = new Date();
+// const now = new Date();
 
 // console.log(format(now, "es"));
 // console.log(format(now, "en"));
-console.log(format(now, "es", { dateStyle: "long" }));
+// console.log(format(now, "es", { dateStyle: "long" }));
 // console.log(format(now, "es", { weekday: "short", day: "numeric" }));
+
+function findNaughtyStep(original, modified) {
+  for (let i = 0; i < Math.max(original.length, modified.length); i++) {
+    if (
+      original[i] !== modified[i] ||
+      i === original.length ||
+      i === modified.length
+    ) {
+      return modified[i] || original[i];
+    }
+  }
+
+  return "";
+}
+
+const original = "abcd";
+const modified = "abcde";
+console.log(findNaughtyStep(original, modified)); // Should return "e"
