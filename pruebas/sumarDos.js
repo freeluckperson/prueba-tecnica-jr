4603889c2abc1d9f8f0c0becc92c84481cbdc7e2
erickubuntu <gestionecesaria@gmail.com>
2024-06-19 12:51:28 -0400

    ejercicio sumarDos

--- pruebas/sumarDos.js
+++ pruebas/sumarDos.js
@@ -3,21 +3,7 @@
 // y como segundo parametro un numero que sera el resultado de sumar
 // dos valores del array
 
-//Ejemplo
-// sumarDos([1, 3, 9, 8, 2], 10)  [8, 2] --> resultado
-
-// function sumarDos(arr, num) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length; j++) {
-//       if (arr[i] + arr[j] === num) {
-//         return [arr[i], arr[j]];
-//       }
-//     }
-//   }
-//   return "No Hay elementos que cumplan la condicion";
-// }
-
-function sumarDos(arr, num) {
+const sumarDos = (arr, num) => {
   for (const num1 of arr) {
     for (const num2 of arr) {
       if (num1 + num2 === num) {
@@ -26,9 +12,11 @@ function sumarDos(arr, num) {
     }
   }
   return "No Hay elementos que cumplan la condicion";
-}
+};
 
-const resultado = sumarDos([1, 3, 9, 8, 2], 10);
-console.log(resultado);
+// const resultado = sumarDos([1, 3, 9, 8, 2], 10);
+// const resultado2 = sumarDos([1, 2, 3, 4], 6);
+// console.log(resultado);
+// console.log(resultado2);
 
-
+module.exports = sumarDos;
